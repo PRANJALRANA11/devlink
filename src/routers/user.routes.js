412ded77c05f2a2310b787388aacba5263c8a574
Pranjal Rana <129268721+PRANJALRANA11@users.noreply.github.com>
2024-06-29 10:46:35 +0530

    feat: Add getWatchHistory endpoint
    The `getWatchHistory` endpoint is added to the `user.controllers.js` file. This endpoint retrieves the watch history for a user by performing an aggregation query on the `User` collection. It fetches the user's watch history, including the video details and the owner's information. This feature allows users to view their previously watched videos.

--- src/routers/user.routes.js
+++ src/routers/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { UserRegister,UserLogin ,UserLogout,refreshAccessToken,changeCurrentPassword,getCurrentUser,updateAccountDetails,updateUserAvatar,updateUserCoverImage} from "../controllers/user.controllers.js";
+import { UserRegister,UserLogin ,UserLogout,refreshAccessToken,changeCurrentPassword,getCurrentUser,updateAccountDetails,updateUserAvatar,updateUserCoverImage,getUserChannelProfile,getWatchHistory} from "../controllers/user.controllers.js";
 import { upload } from "../middlewares/multer.middlewares.js";
 import verifyUser from "../middlewares/auth.middlewares.js";
 
@@ -9,11 +9,12 @@ router.post("/register", upload.fields([{ name: "avatar", maxCount: 1 }, { name:
 router.post("/login", UserLogin);
 router.post("/logout",verifyUser, UserLogout);
 router.route("/refresh-token").post(refreshAccessToken)
-router.route("/change-password").post(verifyJWT, changeCurrentPassword)
-router.route("/current-user").get(verifyJWT, getCurrentUser)
-router.route("/update-account").patch(verifyJWT, updateAccountDetails)
+router.route("/change-password").post(verifyUser, changeCurrentPassword)
+router.route("/current-user").get(verifyUser, getCurrentUser)
+router.route("/update-account").patch(verifyUser, updateAccountDetails)
 
-router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
-router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
-
+router.route("/avatar").patch(verifyUser, upload.single("avatar"), updateUserAvatar)
+router.route("/cover-image").patch(verifyUser, upload.single("coverImage"), updateUserCoverImage)
+router.route("/c/:username").get(verifyUser, getUserChannelProfile)
+router.route("/history").get(verifyUser, getWatchHistory)
 export default router;
